a07cb3264cf51b2dee34c97c307ff977228afc8c
Jan Bouma <acropia@gmail.com>
2020-09-03 18:30:55 +0200

    Various refactoring to Bootstrap

--- mt.php
+++ mt.php
@@ -1,101 +1,159 @@
 <?php
 $physicalMemory = 1;
 function human_readable($number) {
-	if ($number > 1024*1024*1024) {
-		return round($number / (1024*1024*1024),2)." G";
-	}
-	elseif ($number > 1024*1024) {
-		return round($number / (1024*1024),2)." M";
-	}
-	elseif ($number > 1024) {
-		return round($number / (1024),2)." K";
-	}
-	else {
-		return $number ." bytes";
-	}
+    if ($number > 1024 * 1024 * 1024) {
+        return round($number / (1024 * 1024 * 1024), 2) . " G";
+    }
+    elseif ($number > 1024 * 1024) {
+        return round($number / (1024 * 1024), 2) . " M";
+    }
+    elseif ($number > 1024) {
+        return round($number / (1024), 2) . " K";
+    }
+    else {
+        return $number . " bytes";
+    }
 }
+
 function human_readable_time($seconds) {
-	return floor($seconds/86400)." days, ".($seconds/3600%24)." hrs";
+    return floor($seconds / 86400) . " days, " . ($seconds / 3600 % 24) . " hrs, ". ($seconds/60%60)." min";
 }
+
+$user = getenv("MTP_USER");
+$pass = getenv("MTP_PASS");
+$host = getenv("MTP_HOST");
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+/* Server version */
+$stmt = $pdo->query("SELECT VERSION() AS version");
+$version = $stmt->fetch()['version'];
+$versionParts = explode(".", $version);
+$majorVersion = $versionParts[0] . "." . $versionParts[1];
+
+/* Global status */
+$globalStatus = [];
+$stmt = $pdo->query('SHOW GLOBAL STATUS');
+while ($row = $stmt->fetch()) {
+    $globalStatus[$row['Variable_name']] = $row['Value'];
+}
+
+/* Global variables */
+$globalVariables = [];
+$stmt = $pdo->query('SHOW GLOBAL VARIABLES');
+while ($row = $stmt->fetch()) {
+    $globalVariables[$row['Variable_name']] = $row['Value'];
+}
+
+/* InnoDB Index Length */
+$innodbIndexLength = 0;
+$stmt = $pdo->query("SELECT IFNULL(SUM(INDEX_LENGTH),0) AS index_length from information_schema.TABLES where ENGINE='InnoDB'");
+$innodbIndexLength = $stmt->fetch()['index_length'];
+
+/* InnoDB Data Length */
+$innodbDataLength = 0;
+$stmt = $pdo->query("SELECT IFNULL(SUM(DATA_LENGTH),0) AS data_length from information_schema.TABLES where ENGINE='InnoDB'");
+$innodbDataLength = $stmt->fetch()['data_length'];
+
+
+$questions = $globalStatus['Questions'];
+$uptime = $globalStatus['Uptime'];
+$avgQps = $questions / $uptime;
+$threadsConnected = $globalStatus['Threads_connected'];
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-        <!-- Bootstrap CSS -->
-	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
-	        <title>Hello, world!</title>
-<style>
-.table-sm td {
-	font-size: .9em;
-	padding: 1px;
-}
-</style>
-		  </head>
-		    <body>
-<?php
-$host = '127.0.0.1';
-$user = '';
-$pass = '';
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-	PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-	PDO::ATTR_EMULATE_PREPARES   => false,
-];
-try {
-	$pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-	throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
-
-
-$globalStatus = [];
-$globalVariables = [];
-
-$stmt = $pdo->query('SHOW GLOBAL STATUS');
-while ($row = $stmt->fetch()) {
-	$globalStatus[$row['Variable_name']] = $row['Value'];
-}
-
-
-$stmt = $pdo->query('show global variables');
-while ($row = $stmt->fetch()) {
-	$globalVariables[$row['Variable_name']] = $row['Value'];
-}
-
-$innodbIndexes = 0;
-$stmt = $pdo->query("SELECT IFNULL(SUM(INDEX_LENGTH),0) AS index_count from information_schema.TABLES where ENGINE='InnoDB'");
-while ($row = $stmt->fetch()) {
-	$innodbIndexes = $row['index_count'];
-}
-$innodbDataLength = 0;
-$stmt = $pdo->query("SELECT IFNULL(SUM(DATA_LENGTH),0) AS data_length from information_schema.TABLES where ENGINE='InnoDB'");
-while ($row = $stmt->fetch()) {
-	$innodbDataLength = $row['data_length'];
-}
-
-?>
-<h1>Uptime</h1>
-<p>Uptime: <?php echo $globalStatus['Uptime']; ?></p>
-<p><?php echo human_readable_time($globalStatus['Uptime']); ?></p>
-<p>Avg. qps: <?php echo round($globalStatus['Questions']/$globalStatus['Uptime'],1); ?></p>
-<p>Questions: <?php echo $globalStatus['Questions']; ?></p>
-<p>Threads: <?php echo $globalStatus['Threads_connected']; ?></p>
-<?php
-if ($globalStatus['Uptime'] > 172800) {
-	echo "Save";
-}
-else {
-	echo "Warning";
-}
-?>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>MySQL-Tuner-PHP</title>
+    <style>
+        .table-sm td {
+            font-size: .9em;
+            padding: 1px;
+        }
+    </style>
+</head>
+<body>
+<div class='container'>
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">MySQL-Tuner-PHP</a>
+    </nav>
+        <div class='row'>
+            <div class='col-sm'>
+               <table class='table table-sm'>
+                   <tr>
+                       <td>Host:</td>
+                       <td><?= $host ?></td>
+                   </tr>
+                   <tr>
+                       <td>User:</td>
+                       <td><?= $user ?></td>
+                   </tr>
+               </table>
+            </div>
+            <div class='col-sm'>
+                <table class='table table-sm'>
+                    <tr>
+                        <td>Server version:</td>
+                        <td><?= $version ?></td>
+                    </tr>
+                    <tr>
+                        <td>Major version:</td>
+                        <td><?= $majorVersion ?></td>
+                    </tr>
+                </table>
+            </div>
+            <div class='col-sm'>
+                <table class='table table-sm'>
+                    <tr>
+                        <td>Uptime:</td>
+                        <td><?= $uptime ?> seconds</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td><?= human_readable_time($uptime) ?></td>
+                    </tr>
+                    <tr>
+                        <td>Questions:</td>
+                        <td><?= $questions ?></td>
+                    </tr>
+                    <tr>
+                        <td>Avg. qps:</td>
+                        <td><?= round($avgQps,1) ?></td>
+                    </tr>
+                    <tr>
+                        <td>Threads connected:</td>
+                        <td><?= $threadsConnected ?></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <div class='row'>
+            <?php
+                if ($globalStatus['Uptime'] < 172800) {
+            ?>
+            <div class="alert alert-danger" role="alert">
+                Server has not been running for at least 48hrs. It may not be safe to use these recommendations!
+            </div>
+            <?php
+                }
+            ?>
+        </div>
+</div>
 <h1>Slow queries</h1>
 <p>Slow queries: <?php echo $globalStatus['Slow_queries']; ?></p>
 
@@ -107,16 +165,16 @@ else {
 <p>Threads cached: <?php echo $globalStatus['Threads_cached']; ?></p>
 <p>Thread cache size: <?php echo $globalVariables['thread_cache_size']; ?></p>
 <?php
-$historicThreadsPerSec = $globalStatus['Threads_created']/$globalStatus['Uptime'];
+$historicThreadsPerSec = $globalStatus['Threads_created'] / $globalStatus['Uptime'];
 ?>
-<p>Historic threads per sec: <?php echo round($historicThreadsPerSec,4); ?></p>
+<p>Historic threads per sec: <?php echo round($historicThreadsPerSec, 4); ?></p>
 
 <?php
 if ($historicThreadsPerSec > 2) {
-	echo "Warning";
+    echo "Warning";
 }
 else {
-	echo "Save";
+    echo "Save";
 }
 ?>
 
@@ -126,22 +184,22 @@ $maxConnections = $globalVariables['max_connections'];
 $maxUsedConnections = $globalStatus['Max_used_connections'];
 $threadsConnected = $globalStatus['Threads_connected'];
 
-$connectionsRatio = ($maxUsedConnections*100/$maxConnections);
+$connectionsRatio = ($maxUsedConnections * 100 / $maxConnections);
 ?>
 <p>Max connections: <?php echo $maxConnections; ?></p>
 <p>Threads connected: <?php echo $threadsConnected; ?></p>
 <p>Historic max used connections: <?php echo $maxUsedConnections; ?></p>
-<p>Connections ratio: <?php echo round($connectionsRatio,1); ?></p>
+<p>Connections ratio: <?php echo round($connectionsRatio, 1); ?></p>
 
 <?php
 if ($connectionsRatio > 85) {
-	echo "Warning: raise please";
+    echo "Warning: raise please";
 }
 elseif ($connectionsRatio < 10) {
-	echo "Warning: lower please";
+    echo "Warning: lower please";
 }
 else {
-	echo "Save";
+    echo "Save";
 }
 ?>
 
@@ -158,29 +216,29 @@ $innodbLogFilesInGroup = $globalVariables['innodb_log_files_in_group'];
 $innodbSafeBinlog = $globalVariables['innodb_safe_binlog'];
 $innodbThreadConcurrency = $globalVariables['innodb_thread_concurrency'];
 ?>
-<p>InnoDB indexes: <?php echo $innodbIndexes; ?></p>
+<p>InnoDB indexes: <?php echo $innodbIndexLength; ?></p>
 <?php
-if ($innodbIndexes > 0) {
-	$innodbBufferPoolPagesData = $globalStatus['Innodb_buffer_pool_pages_data'];
-	$innodbBufferPoolPagesMisc = $globalStatus['Innodb_buffer_pool_pages_misc'];
-	$innodbBufferPoolPagesFree = $globalStatus['Innodb_buffer_pool_pages_free'];
-	$innodbBufferPoolPagesTotal = $globalStatus['Innodb_buffer_pool_pages_total'];
-	$innodbBufferPoolReadAheadSeq = $globalStatus['Innodb_buffer_pool_read_ahead_seq'];
-	$innodbBufferPoolReadRequests = $globalStatus['Innodb_buffer_pool_read_requests'];
-	$innodbOsLogPendingFsyncs = $globalStatus['Innodb_os_log_pending_fsyncs'];
-	$innodbOsLogPendingWrites = $globalStatus['Innodb_os_log_pending_writes'];
-	$innodbLogWaits = $globalStatus['Innodb_log_waits'];
-	$innodbRowLockTime = $globalStatus['Innodb_row_lock_time'];
-	$innodbRowLockWaits = $globalStatus['Innodb_row_lock_waits'];
+if ($innodbIndexLength > 0) {
+    $innodbBufferPoolPagesData = $globalStatus['Innodb_buffer_pool_pages_data'];
+    $innodbBufferPoolPagesMisc = $globalStatus['Innodb_buffer_pool_pages_misc'];
+    $innodbBufferPoolPagesFree = $globalStatus['Innodb_buffer_pool_pages_free'];
+    $innodbBufferPoolPagesTotal = $globalStatus['Innodb_buffer_pool_pages_total'];
+    $innodbBufferPoolReadAheadSeq = $globalStatus['Innodb_buffer_pool_read_ahead_seq'];
+    $innodbBufferPoolReadRequests = $globalStatus['Innodb_buffer_pool_read_requests'];
+    $innodbOsLogPendingFsyncs = $globalStatus['Innodb_os_log_pending_fsyncs'];
+    $innodbOsLogPendingWrites = $globalStatus['Innodb_os_log_pending_writes'];
+    $innodbLogWaits = $globalStatus['Innodb_log_waits'];
+    $innodbRowLockTime = $globalStatus['Innodb_row_lock_time'];
+    $innodbRowLockWaits = $globalStatus['Innodb_row_lock_waits'];
 
-	$innodbBufferPoolFreePct = $innodbBufferPoolPagesFree*100/$innodbBufferPoolPagesTotal;
+    $innodbBufferPoolFreePct = $innodbBufferPoolPagesFree * 100 / $innodbBufferPoolPagesTotal;
 
-?>
-<p>InnoDB buffer pool pages free: <?php echo $innodbBufferPoolPagesFree; ?></p>
-<p>InnoDB index space: <?php echo human_readable($innodbIndexes); ?></p>
-<p>InnoDB data space: <?php echo human_readable($innodbDataLength); ?></p>
-<p>InnoDB buffer pool free: <?php echo round($innodbBufferPoolFreePct,1); ?> %</p>
-<?php
+    ?>
+    <p>InnoDB buffer pool pages free: <?php echo $innodbBufferPoolPagesFree; ?></p>
+    <p>InnoDB index space: <?php echo human_readable($innodbIndexLength); ?></p>
+    <p>InnoDB data space: <?php echo human_readable($innodbDataLength); ?></p>
+    <p>InnoDB buffer pool free: <?php echo round($innodbBufferPoolFreePct, 1); ?> %</p>
+    <?php
 }
 ?>
 <p>InnoDB buffer pool size: <?php echo human_readable($innodbBufferPoolSize); ?></p>
@@ -194,25 +252,25 @@ $threadStack = $globalVariables['thread_stack'];
 $maxConnections = $globalVariables['max_connections'];
 $joinBufferSize = $globalVariables['join_buffer_size'];
 $tmpTableSize = $globalVariables['tmp_table_size'];
-$max_heap_table_size = $globalVariables['max_heap_table_size'];
+$maxHeapTableSize = $globalVariables['max_heap_table_size'];
 $logBin = $globalVariables['log_bin'];
 $maxUsedConnections = $globalStatus['Max_used_connections'];
 
 if ($logBin = "ON") {
-	$binlogCacheSize = $globalVariable['binlog_cache_size'];
+    $binlogCacheSize = $globalVariables['binlog_cache_size'];
 }
 else {
-	$binlogCacheSize = 0;
+    $binlogCacheSize = 0;
 }
 if ($maxHeapTableSize < $tmpTableSize) {
-	$effectiveTmpTableSize = $maxHeapTableSize;
+    $effectiveTmpTableSize = $maxHeapTableSize;
 }
 else {
-	$effectiveTmpTableSize = $tmpTableSize;
+    $effectiveTmpTableSize = $tmpTableSize;
 }
 
-$perThreadBuffers = ($readBufferSize + $readRndBufferSize + $softBufferSize + $threadStack + $joinBufferSize + $binlogCacheSize) * $maxConnections;
-$perThreadMaxBuffers = ($readBufferSize + $readRndBufferSize + $softBufferSize + $threadStack + $joinBufferSize + $binlogCacheSize) * $maxUsedConnections;
+$perThreadBuffers = ($readBufferSize + $readRndBufferSize + $sortBufferSize + $threadStack + $joinBufferSize + $binlogCacheSize) * $maxConnections;
+$perThreadMaxBuffers = ($readBufferSize + $readRndBufferSize + $sortBufferSize + $threadStack + $joinBufferSize + $binlogCacheSize) * $maxUsedConnections;
 
 $innodbBufferPoolSize = $globalVariables['innodb_buffer_pool_size'];
 if (empty($innodbBufferPoolSize)) $innodbBufferPoolSize = 0;
@@ -233,7 +291,7 @@ $globalBuffers = $innodbBufferPoolSize + $innodbAdditionalMemPoolSize + $innodbL
 $maxMemory = $globalBuffers + $perThreadMaxBuffers;
 $totalMemory = $globalBuffers + $perThreadBuffers;
 
-$pctOfSysMem = $totalMemory*100/$physicalMemory;
+$pctOfSysMem = $totalMemory * 100 / $physicalMemory;
 ?>
 <p>Mem Memory Ever Allocated: <?php echo human_readable($maxMemory); ?></p>
 <p>Configured Max Per-thread Buffers: <?php echo human_readable($perThreadBuffers); ?></p>
@@ -243,10 +301,10 @@ $pctOfSysMem = $totalMemory*100/$physicalMemory;
 
 <?php
 if ($pctOfSysMem > 90) {
-	echo "Warning";
+    echo "Warning";
 }
 else {
-	echo "Save";
+    echo "Save";
 }
 ?>
 
@@ -257,23 +315,23 @@ $keyReads = $globalStatus['Key_reads'];
 $keyBlocksUsed = $globalStatus['Key_blocks_used'];
 $keyBlocksUnused = $globalStatus['Key_blocks_unused'];
 $keyCacheBlockSize = $globalVariables['key_cache_block_size'];
-$keyBufferSize = $globaVariables['key_buffer_size'];
-$dataDir = $globaVariables['datadir'];
-$versionCompileMachine = $globaVariables['version_compile_machine'];
+$keyBufferSize = $globalVariables['key_buffer_size'];
+$dataDir = $globalVariables['datadir'];
+$versionCompileMachine = $globalVariables['version_compile_machine'];
 
 if ($keyReads == 0) {
-	echo "Warning";
-	$keyCacheMissRate = 0;
-	$keyBufferFree = $keyBlocksUnused * $keyCacheBlockSize / $keyBufferSize * 100;
+    echo "Warning";
+    $keyCacheMissRate = 0;
+    $keyBufferFree = $keyBlocksUnused * $keyCacheBlockSize / $keyBufferSize * 100;
 }
 else {
-	$keyCacheMissRate = $keyReadRequests / $keyReads;
-	if (!$keyBlocksUnused) {
-		$keyBufferFree = $keyBlocksUnused * $keyCacheBlockSize / $keyBufferSize * 100;
-	}
-	else {
-		$keyBufferFree = "Unknown";
-	}
+    $keyCacheMissRate = $keyReadRequests / $keyReads;
+    if (!$keyBlocksUnused) {
+        $keyBufferFree = $keyBlocksUnused * $keyCacheBlockSize / $keyBufferSize * 100;
+    }
+    else {
+        $keyBufferFree = "Unknown";
+    }
 }
 ?>
 <p>Current key buffer size: <?php echo human_readable($keyBufferSize); ?></p>
@@ -282,27 +340,35 @@ else {
 
 
 <h1>GLOBAL STATUS</h1>
-<a class="btn btn-primary" data-toggle="collapse" href="#collapseStatus" role="button" aria-expanded="false" aria-controls="collapseStatus">Show status</a>
+<a class="btn btn-primary" data-toggle="collapse" href="#collapseStatus" role="button" aria-expanded="false"
+   aria-controls="collapseStatus">Show status</a>
 <table class='table table-sm collapse' id='collapseStatus'>
-<?php
-foreach ($globalStatus as $globalStatusName=>$globalStatusValue) {
-	echo "<tr><td>".$globalStatusName."</td><td>".$globalStatusValue."</td></tr>\n";
-}
-?>
+    <?php
+    foreach ($globalStatus as $globalStatusName => $globalStatusValue) {
+        echo "<tr><td>" . $globalStatusName . "</td><td>" . $globalStatusValue . "</td></tr>\n";
+    }
+    ?>
 </table>
 <h1>GLOBAL VARIABLES</h1>
-<a class="btn btn-primary" data-toggle="collapse" href="#collapseVariables" role="button" aria-expanded="false" aria-controls="collapseVariables">Show variables</a>
+<a class="btn btn-primary" data-toggle="collapse" href="#collapseVariables" role="button" aria-expanded="false"
+   aria-controls="collapseVariables">Show variables</a>
 <table class='table table-sm collapse' id='collapseVariables'>
-<?php
-foreach ($globalVariables as $globalVariableName=>$globalVariableValue) {
-	echo "<tr><td>".$globalVariableName."</td><td>".$globalVariableValue."</td></tr>\n";
-}
-?>
+    <?php
+    foreach ($globalVariables as $globalVariableName => $globalVariableValue) {
+        echo "<tr><td>" . $globalVariableName . "</td><td>" . $globalVariableValue . "</td></tr>\n";
+    }
+    ?>
 </table>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+</body>
 </html>
